2f05b22285dd58079e42d7013bd61daae97f6a01
Rafael Ramos de Oliveira Lopes <rafaellopes.dev@gmail.com>
2023-04-07 12:34:05 -0300

    feat: Cria endpoint responsável por mudar o docente de turma.
    A alteração será realizada pelo id do docente e o id da turma que ele passará a ensinar.

--- src/endpoints/updateTeacher.ts
+++ src/endpoints/updateTeacher.ts
@@ -1,9 +1,21 @@
 import { Request, Response } from "express";
+import { UpdateTeacher } from "../models/UpdateTeacher";
+import connection from "../data/connection";
 
 export const moveTeacherToDifferentClass = async (req: Request, res: Response): Promise<void> => {
     let errorCode = 400;
     try {
+        const input: UpdateTeacher = {
+            docente_id: req.body.docente_id,
+            turma_id: req.body.turma_id
+        };
 
+        await connection.raw(`
+        UPDATE DOCENTE 
+        SET turma_id = ${input.turma_id}
+        WHERE id = ${input.docente_id}
+        `)
+        res.status(200).send({ message: "As informações foram atualizadas com sucesso!" })
     } catch (error: any) {
         res.status(errorCode).send({ message: error.message })
     };
