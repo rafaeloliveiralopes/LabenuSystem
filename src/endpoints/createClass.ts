263bb1616d2ca55a5d96ca28c5b959518e4d3527
Rafael Ramos de Oliveira Lopes <rafaellopes.dev@gmail.com>
2023-07-27 01:21:10 -0300

    fix: Corrige path para o objeto `Class`.

--- src/endpoints/createClass.ts
+++ src/endpoints/createClass.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { CLASS_TYPE, Class } from "../models/Class";
+import { CLASS_TYPE, Class } from "../models/class";
 import connection from "../data/connection";
 
 export const classCreate = async (req: Request, res: Response): Promise<void> => {
