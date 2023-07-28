35ae48974196dfbbc13e4acfc6081c9d5669ce46
Rafael Ramos de Oliveira Lopes <rafaellopes.dev@gmail.com>
2023-07-28 11:40:20 -0300

    feat(fix): Cria tratamento de erro com mensagem caso o usuário tente registar um estudante com uid=197609(rafae) gid=197609 groups=197609 que já foi registrado.

--- src/endpoints/createTeacher.ts
+++ src/endpoints/createTeacher.ts
@@ -2,54 +2,71 @@ import { Request, Response } from "express";
 import { SPECIALITY, Teacher } from "../models/teacher";
 import connection from "../data/connection";
 
-export const teacherCreate = async (req: Request, res: Response): Promise<void> => {
-    let errorCode = 400;
-    try {
-        const input: Teacher = {
-            id: req.body.id,
-            nome: req.body.nome,
-            email: req.body.email,
-            data_nasc: req.body.data_nasc,
-            especialidades: req.body.especialidades,
-            turma_id: req.body.turma_id
-        };
+export const teacherCreate = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  let errorCode = 400;
+  try {
+    const input: Teacher = {
+      id: req.body.id,
+      nome: req.body.nome,
+      email: req.body.email,
+      data_nasc: req.body.data_nasc,
+      especialidades: req.body.especialidades,
+      turma_id: req.body.turma_id,
+    };
 
-        if (
-            !input.id ||
-            !input.nome ||
-            !input.email ||
-            !input.data_nasc ||
-            input.especialidades.length < 1
-        ) {
-            errorCode = 422;
-            throw new Error("Verifique se todos os campos foram preenchidos corretamente");
-        }
-
-        const existingTeacherEmail = await connection("DOCENTE")
-            .where("email", input.email)
-            .first();
-
-        if (existingTeacherEmail) {
-            throw new Error("Este email já foi cadastrado. Registre um email diferente.");
-        }
-
-        await connection("DOCENTE").insert({
-            id: input.id,
-            nome: input.nome,
-            email: input.email,
-            data_nasc: input.data_nasc,
-            turma_id: input.turma_id
-        });
-
-        for (let especialidade of input.especialidades) {
-            await connection("DOCENTE_ESPECIALIDADE").insert({
-                docente_id: input.id,
-                especialidade_id: SPECIALITY[especialidade]
-            });
-        }
-
-        res.status(201).send({ message: `Docente ${input.nome}, criado com sucesso!` });
-    } catch (error: any) {
-        res.status(errorCode).send({ message: error.message });
+    if (
+      !input.id ||
+      !input.nome ||
+      !input.email ||
+      !input.data_nasc ||
+      input.especialidades.length < 1
+    ) {
+      errorCode = 422;
+      throw new Error(
+        "Verifique se todos os campos foram preenchidos corretamente"
+      );
     }
+
+    const existingTeacherEmail = await connection("DOCENTE")
+      .where("email", input.email)
+      .first();
+
+    if (existingTeacherEmail) {
+      throw new Error(
+        "Este email já foi cadastrado. Registre um email diferente."
+      );
+    }
+
+    const existingTeacherId = await connection("DOCENTE")
+      .where("id", input.id)
+      .first();
+
+    if (existingTeacherId) {
+      throw new Error("Este ID já foi cadastrado. Registre um ID diferente.");
+    }
+
+    await connection("DOCENTE").insert({
+      id: input.id,
+      nome: input.nome,
+      email: input.email,
+      data_nasc: input.data_nasc,
+      turma_id: input.turma_id,
+    });
+
+    for (let especialidade of input.especialidades) {
+      await connection("DOCENTE_ESPECIALIDADE").insert({
+        docente_id: input.id,
+        especialidade_id: SPECIALITY[especialidade],
+      });
+    }
+
+    res
+      .status(201)
+      .send({ message: `Docente ${input.nome}, criado com sucesso!` });
+  } catch (error: any) {
+    res.status(errorCode).send({ message: error.message });
+  }
 };
